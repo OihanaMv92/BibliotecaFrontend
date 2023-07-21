3d8f7d0626baa2faf8dec73be28c8a0639608ac2
oihana <oihanamv92@gmail.com>
2023-07-21 08:37:30 +0200

    prueba

---
+++ src/main/java/es/fujitsu/frontend/FrontendApplication.java
@@ -0,0 +1,13 @@
+package es.fujitsu.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/fujitsu/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fujitsu.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
